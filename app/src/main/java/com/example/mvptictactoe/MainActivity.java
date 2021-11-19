e160703e98ddd4b6a89a2e53a7d260e5be5c2b08
U180709030 <iremelifdemir@posta.mu.edu.tr>
2021-11-19 22:12:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mvptictactoe/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.mvptictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements BoardView {
+    BoardPresenter presenter;
+    TableLayout boardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        presenter = new BoardPresenter(this);
+        boardView = findViewById(R.id.board);
+        for (byte row=0; row<3; row++){
+            TableRow tableRow = (TableRow)boardView.getChildAt(row);
+            for (byte col=0; col<3; col++){
+                Button button = (Button)tableRow.getChildAt(col);
+                BoardPresenter.CellClickListener buttonListener = new BoardPresenter.CellClickListener(presenter,row,col);
+                button.setOnClickListener(buttonListener);
+            }
+        }
+    }
+
+    @Override
+    public void newGame() {
+        for (byte row=0; row<3; row++) {
+            TableRow tableRow = (TableRow) boardView.getChildAt(row);
+            for (byte col = 0; col < 3; col++) {
+                Button button = (Button) tableRow.getChildAt(col);
+                button.setText("");
+                button.setEnabled(true);
+            }
+        }
+    }
+
+    @Override
+    public void putSymbol(char symbol, byte row, byte col) {
+        TableRow tableRow = (TableRow) boardView.getChildAt(row);
+        Button button = (Button) tableRow.getChildAt(col);
+        button.setText(Character.toString(symbol));
+    }
+
+    @Override
+    public void gameEnded(byte winner) {
+        for (byte row=0; row<3; row++) {
+            TableRow tableRow = (TableRow) boardView.getChildAt(row);
+            for (byte col = 0; col < 3; col++) {
+                Button button = (Button) tableRow.getChildAt(col);
+                button.setEnabled(false);
+            }
+        }
+
+    }
+    public void invalidPlay(byte row,byte col){
+        Toast.makeText(this,"Invalid Move",Toast.LENGTH_LONG).show();
+    }
+}
